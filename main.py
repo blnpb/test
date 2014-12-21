4afaf39ce5fb2c0e1ee8df69173470c6017d64e4
root <root@fileserver.blnpb.local>
2014-12-21 19:14:25 +0900

    test.pyを新規作成

---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf8 -*-
+from test import Test;
+
+def main():
+    test = Test();
+    test.say();
+
+main();
---
+++ test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf8-*-
+class Test():
+    test = ["hello","こんにちは"];
+
+    def say(self):
+        for item in self.test:
+            print(item);
